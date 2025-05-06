da045c75296f1b9a68948159a8c6a7a7a566b8c3
Jordan Allard <jma6465@rit.edu>
2025-05-06 00:12:57 -0400

    Offline ray tracing project complete!

--- OfflineRayTracer/OfflineRayTracer/Main.cpp
+++ OfflineRayTracer/OfflineRayTracer/Main.cpp
@@ -1,26 +1,76 @@
-#include "DirectXMath.h"
-#include "color.h"
+#include "offlineRT.h"
 
-#include <iostream>
+#include "camera.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "material.h"
+#include "sphere.h"
 
 int main() {
 
-	// Image
+	// ====== World ======
 
-	int image_width = 256;
-	int image_height = 256;
+	hittable_list world;
 
-	// Render
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
-	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
-	for (int j = 0; j < image_height; j++) {
-		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-		for (int i = 0; i < image_width; i++) {
-			auto pixel_color = color(float(i) / (image_width - 1), float(j) / (image_height - 1), 0);
-			write_color(std::cout, pixel_color);
-		}
-	}
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
 
-	std::clog << "\rDone.                                   \n";
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+                else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+                else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+
+	// ====== Camera ======
+
+	camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 1200;
+    cam.samples_per_pixel = 50;
+    cam.max_depth = 50;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0.6;
+    cam.focus_dist = 10.0;
+
+	cam.render(world);
 }
